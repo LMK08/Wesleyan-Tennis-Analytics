88e787379ea33db9413517916244b777097a617c
LKFootball <lucaskimball98@gmail.com>
2024-08-21 16:19:54 -0400

    Updated Building_Dashboard.py with correct app initialization and imports

--- Building_Dashboard.py
+++ Building_Dashboard.py
@@ -5,8 +5,8 @@
 
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
